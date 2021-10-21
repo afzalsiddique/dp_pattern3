da01e5755403627534fafa2cbd008e28068277d9
Afzal Siddique <afzal.siddique99@gmail.com>
2021-10-21 10:28:34 +0600

    lab done

--- src/TestATMMachine.java
+++ src/TestATMMachine.java
@@ -8,18 +8,15 @@ public class TestATMMachine {
         System.out.println("Cash in ATM Machine from proxy $" + atmProxy.getCashInMachine());
         atmProxy.insertCard();
         atmProxy.insertPin(1234);
-        atmProxy.requestCash(200);
+        atmProxy.requestCash(1500);
         atmProxy.ejectCard();
-
-
-        System.out.println("Current ATM State " + atmProxy.getATMState());
 
         System.out.println("Cash in ATM Machine from proxy $" + atmProxy.getCashInMachine());
 
         // not possible
         // atmProxy.setCashInMachine(10000);
 
-        System.out.println("\nSetting cash in the real machine");
+        System.out.println("\nSetting cash in the real machine by the bank");
         realATMMachine.setCashInMachine(10000);
         System.out.println("Cash in ATM Machine from real machine $" + realATMMachine.getCashInMachine());
         System.out.println("Cash in ATM Machine from proxy $" + atmProxy.getCashInMachine());
